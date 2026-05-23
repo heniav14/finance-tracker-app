7f0808887227ac29916b8fabbc898917d4d35aef
heniav14 <henokw14@gmail.com>
2026-05-23 00:11:39 -0500

    Auth Logic complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { Button } from "../components/ui/button";
+import UserDropdown from "../components/ui/user-dropdown"
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import "./globals.css";
+import Link from "next/link";
+import { ChartColumnBigIcon } from "lucide-react";
 
 const poppins = Poppins({
   weight: [
@@ -30,11 +41,36 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${poppins.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">
-        <nav className="bg-primary p-4"></nav>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`${poppins.variable} h-full antialiased`}>
+        <body className="min-h-full flex flex-col">
+          <nav className="bg-primary p-4 text-white h-20 flex items-center justify-between">
+            <Link
+              href="/"
+              className="font-bold text-xl flex gap-1 items-center"
+            >
+              <ChartColumnBigIcon className="text-lime-500" />
+              Next Cash
+            </Link>
+            <div>
+              <Show when="signed-out">
+                <div className="flex items-center">
+                  <Button asChild variant="link" className="text-white">
+                    <SignInButton />
+                  </Button>
+                  <Button asChild variant="link" className="text-white">
+                    <SignUpButton />
+                  </Button>
+                </div>
+              </Show>
+              <Show when="signed-in">
+                <UserDropdown />
+              </Show>
+            </div>
+          </nav>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ components/ui/user-dropdown.tsx
@@ -0,0 +1,16 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function UserDropdown() {
+  return (
+    <UserButton
+      showName
+      appearance={{
+        elements: {
+          userButtonOuterIdentifier: {
+            color: "white",
+          },
+        },
+      }}
+    />
+  );
+}
