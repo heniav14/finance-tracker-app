b19577142948ca145e9314bafcf88fbad585cde9
heniav14 <henokw14@gmail.com>
2026-05-30 16:56:59 -0500

    Transactions page table is completed

--- app/dashboard/transactions/page.tsx
+++ app/dashboard/transactions/page.tsx
@@ -6,6 +6,7 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -13,9 +14,49 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { getTransactionsByMonth } from "@/data/getTransactionsByMonth";
+import { format } from "date-fns";
+import { PencilIcon } from "lucide-react";
 import Link from "next/link";
+import z from "zod";
+import numeral from "numeral";
+import { Badge } from "@/components/ui/badge";
 
-export default function Transactions() {
+const today = new Date();
+const searchSchema = z.object({
+  year: z.coerce
+    .number()
+    .min(today.getFullYear() - 100)
+    .max(today.getFullYear() + 1)
+    .catch(today.getFullYear()),
+  month: z.coerce
+    .number()
+    .min(1)
+    .max(12)
+    .catch(today.getMonth() + 1),
+});
+
+export default async function Transactions({
+  searchParams,
+}: {
+  searchParams: Promise<{ year: string; month?: string }>;
+}) {
+  const searchParamsValues = await searchParams;
+
+  const { month, year } = searchSchema.parse(searchParamsValues);
+
+  const selectedDate = new Date(year, month - 1, 1);
+
+  const transactions = await getTransactionsByMonth({ month, year });
+
   return (
     <div className="w-full py-10 px-4">
       <Breadcrumb className="max-w-4xl mx-auto">
@@ -24,23 +65,84 @@ export default function Transactions() {
             <BreadcrumbLink asChild>
               <Link href="/dashboard">Dashboard</Link>
             </BreadcrumbLink>
-          </BreadcrumbItem> 
+          </BreadcrumbItem>
           <BreadcrumbSeparator />
           <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <Link href="/dashboard/transactions">Transactions</Link>
-            </BreadcrumbLink>
+            <BreadcrumbPage>Transactions</BreadcrumbPage>
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
       <Card className="mt-4 max-w-4xl mx-auto">
         <CardHeader>
-          <CardTitle>Transactions</CardTitle>
-          <CardDescription>
-            These should desplay recent transactions
-          </CardDescription>
+          <CardTitle className="flex justify-between">
+            <span>{format(selectedDate, "MMM yyyy")} Transactions</span>
+            <div>Dropdowns</div>
+          </CardTitle>
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent>
+          <Button asChild>
+            <Link href="/dashboard/transactions/new">New Transaction</Link>
+          </Button>
+          {!transactions?.length && (
+            <p className="text-center py-10 test-lg text-muted-foreground">
+              There are no transactions for this month
+            </p>
+          )}
+          {!!transactions?.length && (
+            <Table className="mt-4">
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Date</TableHead>
+                  <TableHead>Description</TableHead>
+                  <TableHead>Type</TableHead>
+                  <TableHead>Category</TableHead>
+                  <TableHead>Amount</TableHead>
+                  <TableHead />
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {transactions.map((transaction) => (
+                  <TableRow key={transaction.id}>
+                    <TableCell>
+                      {format(transaction.transactionDate, "do MMM yyyy")}
+                    </TableCell>
+                    <TableCell>{transaction.description}</TableCell>
+                    <TableCell className="capitalize">
+                      <Badge
+                        className={
+                          transaction.transactionType === "income"
+                            ? "bg-lime-500"
+                            : "bg-orange-500"
+                        }
+                      >
+                        {transaction.transactionType}
+                      </Badge>
+                    </TableCell>
+                    <TableCell>{transaction.category}</TableCell>
+                    <TableCell>
+                      $ {numeral(transaction.amount).format("0,0[.]00")}
+                    </TableCell>
+                    <TableCell className="text-right">
+                      <Button
+                        variant="outline"
+                        asChild
+                        size="icon"
+                        className="h-7 w-7"
+                        aria-label="Edit Transaction"
+                      >
+                        <Link
+                          href={`/dashboard/transactions/${transaction.id}`}
+                        >
+                          <PencilIcon />
+                        </Link>
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          )}
+        </CardContent>
       </Card>
     </div>
   );
